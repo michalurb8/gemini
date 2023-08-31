c37a707de1a5aa2177054acd7c41bc09d2ba1bf8
Michal Urbanski <michalurb8@gmail.com>
2023-09-01 00:39:15 +0200

    Style xaxis

--- plot_azure.py
+++ plot_azure.py
@@ -36,8 +36,6 @@ ax = plt.gca()
 plt.margins(x=0)
 plt.stackplot(dates, eurbtc, eureth, eur, labels = ['EUR/BTC', 'EUR/ETH', 'EUR'],colors=[(0.94,0.55,0.05), (0.22, 0.21, 0.5), (0.5,0.5,0.5)], edgecolor='black')
 
-xticksep = 4
-plt.xticks([date.today() - timedelta(days=x) for x in range(0, (datetime.today() - dates[0]).days, xticksep)])
 for index, day in enumerate(dates):
     plt.vlines(day, 0, (eureth[index] + eurbtc[index] + eur[index]), color=(0,0,0,0.05), linewidth=1)
     plt.scatter(day, (eureth[index] + eurbtc[index] + eur[index]), c='black', s=8)
@@ -46,13 +44,14 @@ plt.axhline(y=deposit, color=(0.9,0,0,0.15), linewidth=10)
 
 plt.grid(axis='y',alpha=0.3,color='black')
 plt.yticks(range(step, yrange+step, step), size=12)
-ax.xaxis.set_major_locator(mdates.DayLocator(bymonthday=[10, 20]))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y'))
-ax.tick_params(axis='x', which='major', width = 1, length = 3, labelsize = 10, colors='black', labelrotation= 90)
+
+ax.xaxis.set_major_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+ax.tick_params(axis='x', which='major', width = 2, length = 7, labelsize = 20, colors=(0.2, 0, 0), labelrotation= 0)
 
 ax.xaxis.set_minor_locator(mdates.MonthLocator())
 ax.xaxis.set_minor_formatter(mdates.DateFormatter('%b'))
-ax.tick_params(axis='x', which='minor', width = 2, length = 7, labelsize = 20, colors=(0.5,0,0), labelrotation= 80)
+ax.tick_params(axis='x', which='minor', width = 1, length = 5, labelsize = 15, colors='black', labelrotation= 0)
 
 ax.yaxis.set_major_formatter('€{x:1.2f}')
 ax.tick_params(axis='y', labelrotation= 0)
